dfa49aafcfec49765d0fa8c60f713fe5808ba364
MadRobin13 <madrobin1340@gmail.com>
2024-08-21 15:54:05 -0400

    lots of testing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import discord
-import dotenv
+from dotenv import load_dotenv
 import os
 
 model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
@@ -10,16 +10,12 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-async def main():
-    dotenv.load_dotenv();
-    await client.run(os.getenv("TOKEN"));
-
-main()
+load_dotenv();
 
 @client.event
-
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
@@ -28,6 +24,28 @@ async def on_message(message):
 
     if message.attachments:
         for attachment in message.attachments:
+
             await attachment.save(attachment.filename)
-            results = model(attachment.filename)
-            message.channel.send(results)
+            result = model(attachment.filename)
+
+            # testing stuff
+
+            boxes = result.boxes  # Boxes object for bounding box outputs
+            masks = result.masks  # Masks object for segmentation masks outputs
+            keypoints = result.keypoints  # Keypoints object for pose outputs
+            probs = result.probs  # Probs object for classification outputs
+            obb = result.obb  # Oriented boxes object for OBB outputs
+            result.show()  # display to screen
+            result.save(filename="result.jpg")  # save to disk
+
+            ###############
+
+
+            print(result)
+            await message.channel.send(result.plot())
+
+def main():
+    client.run(os.getenv("TOKEN"));
+
+if __name__ == "__main__":
+    main()
